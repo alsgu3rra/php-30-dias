1a72e8738ede2bae6998200e46b9719f06332ec4
Alan L. S. Guerra <alanlimadasilvaguerra23@gmail.com>
2024-05-16 04:34:39 -0300

    utilizando variaveis

---
+++ project/dia-01/variaveis.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <body>
+    <?php
+      $nome = "Alan Guerra";
+      $idade = 22;
+      $estado = "GO";
+      $cor = "Azul";
+    ?>
+
+    <?php 
+      echo "Seu nome é ".$nome."Com ".$idade."Anos de idade"."Morando em ".$estado." Sua cord favorita é ".$cor
+    ?>
+  </body>
+</html>
